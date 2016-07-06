f7d07bd060f15cb98fc0aadc911248838a8807f3
André Busche <andre.busche@gmx.de>
2016-07-06 15:10:50 +0200

    add examples and checks

--- README.md
+++ README.md
@@ -26,3 +26,30 @@ Thereafter, try the following on another console:
 
 and watch for different outputs.
 
+Call update / delete on the REST-Target,
+## on Windows
+
+### update Customers
+
+compare the output of the following examples: 
+
+	curl -X PUT -i -H "Content-Type:application/json" -d "{\"id\":102,\"firstName\":\"Abigail\",\"lastName\":\"Adams\",\"email\":\"aadams@example.com\",\"city\":\"Braintree\",\"state\":\"MA\",\"birthday\":\"1744-11-22\"}" http://localhost:8080/myJersey/customers/102/update
+
+	curl -X PUT -i -H "Content-Type:application/json" -d "{\"id\":102,\"firstName\":\"Abigail\",\"lastName\":\"Adams\",\"email\":\"aadams@example.com\",\"city\":\"Braintree\",\"state\":\"MA\",\"birthday\":\"1744-11-22\"}" http://localhost:8080/myJersey/customers/106/update
+
+Note that this extends from the linked examples above. I added a check for matching customer IDs (106 vs. 102) s.t. the latter fails with an HTTP 400 error.
+
+### Add Customers
+
+Call the following to add Customers
+
+	curl -X POST -i -H "Content-Type: application/json" -d "{\"firstName\":\"Laura\",\"lastName\":\"Stuart\",\"email\":\"lstuart@example.com\",\"city\":\"Braintree\",\"state\":\"MA\",\"birthday\":\"1744-11-22\"}" http://localhost:8080/myapp/customers/add
+
+Internally the parameterless constructor, Customer(), is used. Why this works, is currently magic to me ... I assume this lies in the power of Jackson.
+
+
+
+## on Linux
+
+	curl -X PUT -i -H "Content-Type: application/json" -d '{"id":102, "firstName":"Abigail","lastName":"Adams","email":"aadams@example.com","city":"Braintree","state":"MA","birthday":"1744-11-22"}' http://localhost:8080/myJersey/customers/102/update
+ 
--- src/main/java/net/brunel/jerseyservice/CustomerService.java
+++ src/main/java/net/brunel/jerseyservice/CustomerService.java
@@ -3,15 +3,20 @@ package net.brunel.jerseyservice;
 import java.util.Optional;
 import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.function.Consumer;
+import java.util.function.Predicate;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 @Path("/customers")
 public class CustomerService {
@@ -24,30 +29,30 @@ public class CustomerService {
 	public String getAllCustomers() {
 		return "---Customer List---\n" + cList.stream().map(c -> c.toString()).collect(Collectors.joining("\n"));
 	}
-	
+
 	@GET
 	@Path("/all")
 	@Produces(MediaType.APPLICATION_JSON)
 	public Customer[] getAllCustomersAsJson() {
 		return cList.toArray(new Customer[0]);
-	}                                  
+	}
 
 	@GET
 	@Path("/")
 	@Produces(MediaType.TEXT_HTML)
 	public String getIndex() {
-		final StringBuffer sb = new StringBuffer("<h1>Root Object</h1>"); 
+		final StringBuffer sb = new StringBuffer("<h1>Root Object</h1>");
 		cList.stream().forEach(new Consumer<Customer>() {
 
 			@Override
 			public void accept(Customer c) {
 				sb.append("<a href=\"" + c.getId() + "\">" + c.getLastName() + "</a><br />");
-				
+
 			}
 		});
-		return  sb.toString();
+		return sb.toString();
 	}
-	
+
 	@GET
 	@Path("{id}")
 	@Produces(MediaType.TEXT_PLAIN)
@@ -56,9 +61,7 @@ public class CustomerService {
 		if (match.isPresent()) {
 			return "---Customer---\n" + match.get().toString();
 		}
-		
 		return "Customer not found";
-
 	}
 
 	@GET
@@ -71,5 +74,42 @@ public class CustomerService {
 		}
 		throw new NotFoundException(new JsonError("Error", "Customer " + id + " not found"));
 
+	}
+
+	@POST
+	@Path("/add")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response addCustomer(Customer customer) {
+		cList.add(customer);
+		return Response.status(201).build();
+	}
+
+	@PUT
+	@Path("{id}/update")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response updateCustomer(Customer customer, @PathParam("id") long id) {
+		int matchIdx = 0;
+
+		if (id != customer.getId())
+			return Response.status(Response.Status.BAD_REQUEST).build();
+
+		Optional<Customer> match = cList.stream().filter(c -> c.getId() == customer.getId()).findFirst();
+		if (match.isPresent()) {
+			matchIdx = cList.indexOf(match.get());
+			cList.set(matchIdx, customer);
+			return Response.status(Response.Status.OK).build();
+		}
+
+		return Response.status(Response.Status.NOT_FOUND).build();
+	}
+
+	@DELETE
+	@Path("/remove/{id}")
+	public void deleteCustomer(@PathParam("id") long id) {
+		Predicate<Customer> customer = c -> c.getId() == id;
+
+		if (!cList.removeIf(customer)) {
+			throw new NotFoundException(new JsonError("Error", "Customer " + id + " not found"));
+		}
 	}
 }
